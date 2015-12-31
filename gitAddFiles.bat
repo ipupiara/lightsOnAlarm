69251453c972f769e9e12dbc8c0c5dfa60ddd05f
peter <peter.niederer@sonnenkinder.org>
2015-12-31 10:57:56 +0100

    initial state

---
+++ gitAddFiles.bat
@@ -0,0 +1,7 @@
+git add *.atsln
+git add *.atsuo
+git add lightsOnAlarm/*.cproj
+git add lightsOnAlarm/*.h
+git add lightsOnAlarm/*.c
+git add gitAddFiles.bat
+git status
---
+++ lightsOnAlarm/lightsOnAlarm.c
@@ -0,0 +1,17 @@
+/*
+ * lightsOnAlarm.c
+ *
+ * Created: 31.12.2015 10:56:01
+ *  Author: mira
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
